a7620300b891bbba47cad763a498893673c86627
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2020-09-10 12:47:47 +0300

    before restarting computer

--- js/script.js
+++ js/script.js
@@ -16,15 +16,13 @@ var deltaHr = deltaMin / 60;
     var deltaMin = ((currentMin / 60) * 360) + ((currentSec / 60) * (360 / 60));
     var deltaSec = (currentSec / 60) * 360;
 
-
     setInterval(function () {
         moveThoseArms()
         updateDeltas();
-        console.log('hej')
     }, 1000)
 
     function moveThoseArms() {
-        console.log(currentSec, currentMin, currentHr)
+
         SECONDHAND.style.transform = `rotate(${deltaSec}deg)`
 
         MINUTEHAND.style.transform = `rotate(${deltaMin}deg)`
@@ -37,12 +35,15 @@ var deltaHr = deltaMin / 60;
     function updateDeltas() {
         deltaSec += 360 / 60;
         currentSec += 1
+        //if current second is 60, make it 0 and increase minutes by one
         if (currentSec === 60) {
             currentSec = 0;
             currentMin += 1
+            //if current minute is 60, make it 0 and increase hours by one
             if (currentMin === 60) {
                 currentMin = 0;
                 currentHr += 1;
+                //if current hour is 0, make it 0
                 if (currentHr === 12) {
                     currentHr = 0;
                 }
