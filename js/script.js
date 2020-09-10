ea850137f085caf5a499c4d842951003d86a677c
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2020-09-10 12:34:08 +0300

    clock working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+var HOURHAND = document.getElementById('hour');
+var MINUTEHAND = document.getElementById('minute');
+var SECONDHAND = document.getElementById('second');
+
+var deltaSec = 360 / 60;
+var deltaMin = deltaSec / 60;
+var deltaHr = deltaMin / 60;
+
+(function ($) {
+    var now = new Date();
+    var currentHr = now.getHours();
+    var currentMin = now.getMinutes();
+    var currentSec = now.getSeconds();
+
+    var deltaHr = currentHr * (360 / 12) + ((currentMin / 60) * 30) + ((currentSec / 60 / 60) * 60);
+    var deltaMin = ((currentMin / 60) * 360) + ((currentSec / 60) * (360 / 60));
+    var deltaSec = (currentSec / 60) * 360;
+
+
+    setInterval(function () {
+        moveThoseArms()
+        updateDeltas();
+        console.log('hej')
+    }, 1000)
+
+    function moveThoseArms() {
+        console.log(currentSec, currentMin, currentHr)
+        SECONDHAND.style.transform = `rotate(${deltaSec}deg)`
+
+        MINUTEHAND.style.transform = `rotate(${deltaMin}deg)`
+        deltaMin += 360 / 60 / 60;
+
+        HOURHAND.style.transform = `rotate(${deltaHr}deg)`
+        deltaHr += (((360 / 60) / 60) / 12);
+    }
+
+    function updateDeltas() {
+        deltaSec += 360 / 60;
+        currentSec += 1
+        if (currentSec === 60) {
+            currentSec = 0;
+            currentMin += 1
+            if (currentMin === 60) {
+                currentMin = 0;
+                currentHr += 1;
+                if (currentHr === 12) {
+                    currentHr = 0;
+                }
+            }
+        }
+    }
+})(window.jQuery)
+
+
