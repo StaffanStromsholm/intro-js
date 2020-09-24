173ff66647646a9f4bedcf1cfa04bf4cd3969c41
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2020-09-24 13:06:57 +0300

    commit berfore checkout

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ document.querySelector('#popup1 .popup .close').addEventListener('click', functi
 function email_subscribepopup(e) {
 
     console.log(document.getElementById('subscribe_pemail').value);
+    validateEmail();
 
     document.getElementById('popup1').style.visibility = 'hidden';
     document.getElementById('popup2').style.visibility = 'visible';
@@ -22,3 +23,34 @@ function email_subscribepopup(e) {
 document.querySelector('#popup2 .popup .close').addEventListener('click', function () {
     document.getElementById('popup2').style.visibility = 'hidden';
 })
+
+//validate email
+function validateEmail() {
+    var _email = getEmail();
+
+    if (checkSpace(_email) === true) {
+        return false;
+    }
+
+    // check for @
+    var atSymbol = _email.indexOf('@');
+    if (atSymbol < 1) {
+        return false;
+    }
+
+    // check if there is a dot located less than 2 symbols away from the @ sign
+    var dot = _email.indexOf('.');
+    if (dot <= atSymbol + 2) {
+        return false;
+    }
+
+    // check that the dot is not at the end
+    if (dot === _email.length - 1) {
+        return false;
+    }
+    return true;
+}
+
+function getEmail() {
+    return document.getElementById('subscribe_pemail').value
+}
