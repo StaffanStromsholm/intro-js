785eabe75c1ccb1cb100e33b2af6b056620619a5
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2020-09-24 12:19:05 +0300

    small refactor

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
+
+//On mouse leave, make popup1 visible
 window.document.addEventListener('mouseleave', function () {
     document.getElementById('popup1').style.visibility = 'visible';
 })
 
+//make popup1 close button Hide popup1
 document.querySelector('#popup1 .popup .close').addEventListener('click', function () {
     document.getElementById('popup1').style.visibility = 'hidden';
 })
 
-document.querySelector('#frm_subscribe a').onclick = email_subscribepopup
-
+//declare email_subscribepopup function
 function email_subscribepopup(e) {
+
     console.log(document.getElementById('subscribe_pemail').value);
 
     document.getElementById('popup1').style.visibility = 'hidden';
     document.getElementById('popup2').style.visibility = 'visible';
 }
 
+//make popup2 close button Hide popup2
 document.querySelector('#popup2 .popup .close').addEventListener('click', function () {
     document.getElementById('popup2').style.visibility = 'hidden';
 })
